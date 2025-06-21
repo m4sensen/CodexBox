87b52cbc6ed24aee739a5778fa1e57ead0cc4bfb
masensen <masensen.dev@proton.me>
2025-06-21 13:06:21 -0400

    New aliase added: git_freshstart.sh

--- bin/install.sh
+++ bin/install.sh
@@ -2,6 +2,8 @@ install() {
 
 copy_and_hide_if_exists "$codexBoxDir/config/aliases/user_bash_aliases" "$HOME"
 
+copy_and_hide_if_exists "$codexBoxDir/config/aliases/user_bash_aliases" "$HOME"
+
 setup_aliases_editor
 
 require_file ~/.bashrc && log "🔁 Aliases reloaded."
